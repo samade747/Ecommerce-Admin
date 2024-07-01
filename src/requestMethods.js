ee70ce3976d3e52ae1717886e20cf7b438342750
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-02 01:14:34 +0500

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,6 +5,8 @@ const Token =
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzQxYzIyYjQ1YzI0MzkwNzU1NjU5ZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjQ3NzI3N30.0n9h7k6kzS7JqSf6Eo7H9OuZ6pWkXyZpV'
 
 
+
+
 export const publicRequest = axios.create({ 
     baseURL: baseURL 
 });
