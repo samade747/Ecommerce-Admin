af0e799b90f3cb0dc951ce14d66e88540f831684
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-13 00:47:11 +0500

    1247am

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,45 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import axios from "axios";
+import { useState, useEffect, useMemo } from "react";
 
 export default function Home() {
+  const [userStats, setUserStats] = useState([]);
+
+  const MONTHS = useMemo(() => [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Agu",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ], []);
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/users.stats");
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "Active User": item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [MONTHS]);
+
+
   return (
     <div className="home">
       <FeaturedInfo />
